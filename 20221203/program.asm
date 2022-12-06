017f452c9ff8f7d329a0aee48d0b4e5dd0c4aad4
SiantyX <siantyx@gmail.com>
2022-12-06 23:16:31 +0100

    day3 done

--- 20221203/program.asm
+++ 20221203/program.asm
@@ -22,36 +22,85 @@ END      EQU   *
 * ------- MAIN START -------
 MAIN     EQU   *
          XR    R6,R6
-LOOP     EQU   *
-         MVI   ITEMAREA,X'00'
+         LHI   R7,2
+GROUPLOOP EQU   *
+         MVI   ITEMAREA,X'40'
          MVC   ITEMAREA+1(L'ITEMAREA-1),ITEMAREA
+         MVI   ITEMAREA2,X'40'
+         MVC   ITEMAREA2+1(L'ITEMAREA2-1),ITEMAREA2
          GET   SYSIN
          LH    R2,0(R1)
          AHI   R2,-4
          AHI   R1,4
-         AR    R2,R1
+         AR    R2,R1             END ADR
 
-LOOP2    EQU   *
+LOOP1    EQU   *
          CR    R1,R2
-         BH    ERROR                END OF STR
+         BNL   ROW2                END OF STR
          LA    R3,ITEMAREA
          XR    R4,R4
          IC    R4,0(R1)
          AR    R3,R4
-         CLC   0(1,R3),=X'00'
-         BNE   DUPLICATE            DUPLICATE
+         STC   R4,0(R3)
+         AHI   R1,1
+         J     LOOP1
+
+VBMVC    MVC   WTOTEXT(0),0(R1)
+* ==========================
+
+ROW2     EQU   *
+         GET   SYSIN
+         LH    R2,0(R1)
+         AHI   R2,-4
+         AHI   R1,4
+         AR    R2,R1             END ADR
+
+LOOP2    EQU   *
+         CR    R1,R2
+         BNL   ROW3                END OF STR
+         LA    R3,ITEMAREA
+         XR    R4,R4
+         IC    R4,0(R1)
+         AR    R3,R4
+         CLC   0(1,R3),=X'40'
+         BNE   DUPLICATE2            DUPLICATE
+         AHI   R1,1
+         J     LOOP2
+         
+DUPLICATE2 EQU *
+         LA    R3,ITEMAREA2
+         AR    R3,R4
          STC   R4,0(R3)
          AHI   R1,1
          J     LOOP2
-DUPLICATE EQU  *
+
+* ==========================
+
+ROW3     EQU   *
+         GET   SYSIN
+         LH    R2,0(R1)
+         AHI   R2,-4
+         AHI   R1,4
+         AR    R2,R1             END ADR
+
+LOOP3    EQU   *
+         CR    R1,R2
+         BNL   ERROR                END OF STR
+         LA    R3,ITEMAREA2
+         XR    R4,R4
+         IC    R4,0(R1)
+         AR    R3,R4
+         CLC   0(1,R3),=X'40'
+         BNE   DUPLICATE3            DUPLICATE
+         AHI   R1,1
+         J     LOOP3
+
+DUPLICATE3 EQU  *
          TR    0(1,R3),CONVERT
-         LB    R4,0(R3)
+         XR    R4,R4
+         IC    R4,0(R3)
          AR    R6,R4
-         CVD   R6,APL8
-         UNPK  WTOTEXT(15),APL8
-         OI    WTOTEXT+14,X'F0'
-         BAL   R14,PRINT
-         J     LOOP
+         J     GROUPLOOP
 
 ERROR    EQU   *
          MVC   WTOTEXT(13),=C'NO DUPLICATE?'
@@ -80,32 +129,33 @@ PRINT    EQU   *
 * --------------------------------------------------------------------   
 SYSIN    DCB   DDNAME=SYSIN,DSORG=PS,MACRF=GL,RECFM=VT,LRECL=80,       +
                EODAD=EOFSYSIN
-SYSOUT   DCB   DDNAME=SYSOUT,DSORG=PS,MACRF=PM,RECFM=FT,LRECL=80                
+SYSOUT   DCB   DDNAME=SYSOUT,DSORG=PS,MACRF=PM,RECFM=FT,LRECL=259               
 *
          LTORG
 *       
 SAVE     DC    18F'0'                          
 *
 WTOBLOCK EQU   *
-         DC    H'84'
+         DC    H'259'
          DC    H'0'
-WTOTEXT  DC    CL80' '
+WTOTEXT  DC    CL255' '
 *
 APL8     DS    PL8
 ITEMAREA DS    XL256
+ITEMAREA2 DS   XL256
 CONVERT  DC    256XL'00'
          ORG   CONVERT+C'a'
          DC    X'010203040506070809'
          ORG   CONVERT+C'j'
-         DC    X'101112131415161718'
+         DC    X'0A0B0C0D0E0F101112'
          ORG   CONVERT+C's'
-         DC    X'1920212223242526'
+         DC    X'131415161718191A'
          ORG   CONVERT+C'A'
-         DC    X'272829303132333435'
+         DC    X'1B1C1D1E1F20212223'
          ORG   CONVERT+C'J'
-         DC    X'363738394041424344'
+         DC    X'2425262728292A2B2C'
          ORG   CONVERT+C'S'
-         DC    X'4546474849505152'
+         DC    X'2D2E2F3031323334'
          ORG
 *
          END                                                                    
--- 20221203/program_partone.asm
+++ 20221203/program_partone.asm
@@ -17,77 +17,67 @@ END      EQU   *
          LA    R15,0                                  
          BR    R14                                                              
 * --------------------------------------------------------------------  
-*
-* A = X = Rock = 1
-* B = Y = Paper = 2
-* C = Z = Scissors = 3
-*
-* Win = 6
-* Draw = 3
-* Loss = 0
-*
-* Number 0 and 2 same = Draw
-* 1 > 3
-* 3 > 2
-* 2 > 1
-*
-* 1 - 2 = -1 win Rock(them) vs Paper(us)
-* 1 - 3 = -2 loss Rock(them) vs Scissors(us)
-*
-* 2 - 3 = -1 win Paper(them) vs Scissors(us)
-* 2 - 1 = 1 loss Paper(them) vs Rock(us)
-*
-* 3 - 2 = 1 loss Scissors(them) vs Paper(us)
-* 3 - 1 = 2 win Scissors(them) vs Rock(us)
-*
-* Score -1 and 2 is win for us
+
 *
 * ------- MAIN START -------
 MAIN     EQU   *
-         USING RPS,R1
-         LHI   R5,0
-         ZAP   APL8,=P'0'
-LOOP     EQU   *   
-         LHI   R2,0
-         LHI   R3,0
-         
-         CVD   R5,APL8
+         XR    R6,R6
+         LHI   R7,2
+LOOP     EQU   *
+         MVI   ITEMAREA,X'00'
+         MVC   ITEMAREA+1(L'ITEMAREA-1),ITEMAREA
+         GET   SYSIN
+         LH    R2,0(R1)
+         AHI   R2,-4
+         AHI   R1,4
+         LR    R10,R2            SAVE ORG VAL
+         AR    R2,R1             END ADR
+*        GET HALFWAY
+         SRDA  R10,32
+         DR    R10,R7
+         LR    R8,R1
+         AR    R8,R11         R8 HALFWAY POINT
+
+LOOP1    EQU   *
+         CR    R1,R8
+         BNL   LOOP2                END OF STR
+         LA    R3,ITEMAREA
+         XR    R4,R4
+         IC    R4,0(R1)
+         AR    R3,R4
+         STC   R4,0(R3)
+         AHI   R1,1
+         J     LOOP1
+
+LOOP2    EQU   *
+         CR    R1,R2
+         BH    ERROR                END OF STR
+         LA    R3,ITEMAREA
+         XR    R4,R4
+         IC    R4,0(R1)
+         AR    R3,R4
+         CLC   0(1,R3),=X'00'
+         BNE   DUPLICATE            DUPLICATE
+         AHI   R1,1
+         J     LOOP2
+
+DUPLICATE EQU  *
+         TR    0(1,R3),CONVERT
+         LB    R4,0(R3)
+         AR    R6,R4
+         CVD   R6,APL8
          UNPK  WTOTEXT(15),APL8
          OI    WTOTEXT+14,X'F0'
          BAL   R14,PRINT
-
-         GET   SYSIN
-
-         IC    R2,THEM
-         S     R2,=X'000000C0'
-
-         IC    R3,US
-         S     R3,=X'000000E6'
-         
-         AR    R5,R3
-         SR    R2,R3
-         C     R2,=F'-1'
-         BE    WIN
-         C     R2,=F'2'
-         BE    WIN
-         C     R2,=F'0'
-         BE    DRAW
-         MVC   WTOTEXT(4),=C'LOSS'
-         BAL   R14,PRINT
          J     LOOP
-WIN      EQU   *
-         AHI   R5,6
-         MVC   WTOTEXT(4),=C'WIN '
+
+ERROR    EQU   *
+         MVC   WTOTEXT(13),=C'NO DUPLICATE?'
          BAL   R14,PRINT
-         J     LOOP
-DRAW     EQU   *
-         AHI   R5,3
-         MVC   WTOTEXT(4),=C'DRAW'
-         BAL   R14,PRINT
-         J     LOOP
+         ABEND 101
 
 EOFSYSIN EQU   *
-         CVD   R5,APL8
+         CVD   R6,APL8
          UNPK  WTOTEXT(15),APL8
          OI    WTOTEXT+14,X'F0'
          BAL   R14,PRINT
@@ -106,7 +96,7 @@ PRINT    EQU   *
 * ------- SUB ROUTINES END ------- 
 
 * --------------------------------------------------------------------   
-SYSIN    DCB   DDNAME=SYSIN,DSORG=PS,MACRF=GL,RECFM=FT,LRECL=3,        +
+SYSIN    DCB   DDNAME=SYSIN,DSORG=PS,MACRF=GL,RECFM=VT,LRECL=80,       +
                EODAD=EOFSYSIN
 SYSOUT   DCB   DDNAME=SYSOUT,DSORG=PS,MACRF=PM,RECFM=FT,LRECL=80                
 *
@@ -119,11 +109,21 @@ WTOBLOCK EQU   *
          DC    H'0'
 WTOTEXT  DC    CL80' '
 *
-APL8     DC    PL8'0'
-*
-RPS      DSECT
-THEM     DS    CL1
-         DS    CL1
-US       DS    CL1
+APL8     DS    PL8
+ITEMAREA DS    XL256
+CONVERT  DC    256XL'00'
+         ORG   CONVERT+C'a'
+         DC    X'010203040506070809'
+         ORG   CONVERT+C'j'
+         DC    X'0A0B0C0D0E0F101112'
+         ORG   CONVERT+C's'
+         DC    X'131415161718191A'
+         ORG   CONVERT+C'A'
+         DC    X'1B1C1D1E1F20212223'
+         ORG   CONVERT+C'J'
+         DC    X'2425262728292A2B2C'
+         ORG   CONVERT+C'S'
+         DC    X'2D2E2F3031323334'
+         ORG
 *
          END                                                                    
